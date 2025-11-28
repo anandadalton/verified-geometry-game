48aab39c3c90c0c51166e2836bc3d11e61f6d46e
Ananda Dalton <ananda.dalton@gmail.com>
2025-11-27 22:31:22 -0800

    Added the state transition function.
    Proved that "dealing" works properly; but in order to feel really convinced we should prove that the major transition function works properly.
    
    Realistically I am going to write the OCaml harness first because I'm eager to see something tangible.

--- theories/GameState.v
+++ theories/GameState.v
@@ -1,51 +1,145 @@
-Require Import Referee Deck Affine.
-From Stdlib Require Import List.
+Require Import Eqb Affine Referee Deck.
+From Stdlib Require Import Arith Bool List.
 Import ListNotations.
 
+
+(* Since we have so many cases where we deal with either exactly three or
+   strictly less than three cards, it makes sense to have custom datatypes
+   (make illegal states impossible to represent). *)
+
+Inductive Selection :=
+| Sel0
+| Sel1 (c1 : Point)
+| Sel2 (c1 c2 : Point).
+
+Inductive CardTriple := mkTriple (c1 c2 c3 : Point).
+
+Inductive SelectResult :=
+| NewSel (s : Selection)
+| TriggerCheck (t : CardTriple).
+
+(* This is mildly annoying compared to fixpoints over lists.
+   As this evolves I should check whether I'm happy with this choice. *)
+Definition handle_click (current : Selection) (p : Point) : SelectResult :=
+  match current with
+  | Sel0 => NewSel (Sel1 p)
+  | Sel1 c1 =>
+    if c1 =? p then NewSel Sel0 else NewSel (Sel2 c1 p)
+  | Sel2 c1 c2 =>
+    if c1 =? p then NewSel (Sel1 c2)
+    else if c2 =? p then NewSel (Sel1 c1)
+    else TriggerCheck (mkTriple c1 c2 p)
+  end.
+
+Inductive Event :=
+| CardClick (card : Point)
+| AnimationOver.
+
 Inductive Phase :=
-| UserSelecting (selection : list Point)
-| AnimatingMatch (selection : list Point)
-| AnimatingFail (selection : list Point)
+| UserSelecting (selection : Selection)
+| AnimatingMatch (triple : CardTriple)
+| AnimatingFail (triple : CardTriple)
+  (* Note that we may end up dealing more than three cards at once. *)
 | AnimatingDeal (new_cards : list Point)
 | GameOver.
 
 Record State : Type := mkState {
-  deck : list Point;
+  (* We use CardTriple because we always deal in groups of three cards. *)
+  deck : list CardTriple;
   board : list Point;
   phase : Phase;
 }.
 
-(* State transitions, and how they occur:
-- The game always starts in UserSelecting [], a board with 12 cards, and a deck
-  with 81-12=69 cards.
-- UserSelecting:
-  + if an already-selected card is clicked, we remove it from selection.
-  + if we click a new card, and we now have three cards, we check if it is a
-    valid triple.
-    * if it is a valid triple, we move to AnimatingMatch (with same selection).
-    * if it is invalid, we move to AnimatingFail (with same selection).
-    * if we don't have three cards after clicking a new card, we just add to
-      the selection.
-- AnimatingMatch: we do not transition out until we receive indication from the
-  OCaml framework that the animation is done. Then we remove the three cards
-  that were selected from the board, and we handle the logic for adding cards.
-  + if there are no more cards in the deck, we don't add any new cards
-    * if there are no more triples on the board, we transition to GameOver.
-    * if there are still triples on the board, we transition to UserSelecting.
-  + if there are still cards in the deck, and there are fewer than 12 cards on
-    the board, we pop three cards from the deck and go to AnimatingDeal with
-    those three cards.
-  + if there are still cards in the deck, and there are more than 12 cards on
-    the board:
-    * if there is a triple on the board, we go directly to UserSelecting [].
-    * if there is no triple on the board, we pop three cards from the deck and go
-      to AnimatingDeal with those three cards.
-- AnimatingFail: we do not transition until we receive indication from the
-  OCaml framework that the animation is done. Then we go to UserSelecting [].
-- AnimatingDeal: we do not transition until we receive indication from the
-  OCaml framework that the animation is done. Then we add those three cards to
-  the board, and we go directly to UserSelecting [].
-- GameOver has no transitions away.
+Fixpoint remove_point (p : Point) (ls : list Point) : list Point :=
+  match ls with
+  | [] => []
+  | (h :: ls') => if (h =? p) then ls' else h :: (remove_point p ls')
+  end.
 
-I think it might also be smart to add some sort of a Reset button so that users
-can begin a new game when the game is over (or in case they give up). *)
+Definition remove_triple (t : CardTriple) (ls : list Point) : list Point :=
+  match t with
+  | mkTriple c1 c2 c3 => remove_point c3 (remove_point c2 (remove_point c1 ls))
+  end.
+
+Definition should_stop (board : list Point) :=
+  referee board && Nat.leb 12 (length board).
+
+(* The third parameter is to track cards that have been dealt;
+   this is primarily a convenience for animating them. *)
+Fixpoint deal (deck : list CardTriple) (board : list Point) (dealt : list Point)
+    : (list CardTriple * list Point * list Point) :=
+  match deck with
+  | [] => ([], board, dealt)
+  | ((mkTriple c1 c2 c3) :: deck') =>
+    let board' := c1 :: c2 :: c3 :: board in
+    let dealt' := c1 :: c2 :: c3 :: dealt in
+      if should_stop board'
+      then (deck', board', dealt')
+      else deal deck' board' dealt'
+  end.
+
+Definition after_deal_invariant (deck_board : list CardTriple * list Point) :=
+  fst deck_board = [] \/ ((has_valid_triple (snd deck_board) /\ 12 <= length (snd deck_board))).
+
+Lemma stopping_condition_sound : forall (remaining_deck : list CardTriple) (board : list Point),
+  should_stop board = true -> after_deal_invariant (remaining_deck, board).
+Proof.
+  intros deck board Hstop.
+  unfold after_deal_invariant, should_stop in *.
+  right.
+  apply andb_prop in Hstop. destruct Hstop as [Hmatch Hlen].
+  split.
+  - apply (reflect_iff _ _ (reflect_has_valid_triple_referee _)); assumption.
+  - apply Nat.leb_le; assumption.
+Qed.
+
+Theorem deal_satisfies_invariant :
+  forall (deck : list CardTriple) (board : list Point) (dealt : list Point),
+  after_deal_invariant (fst (deal deck board dealt)).
+Proof.
+  intros deck.
+  induction deck as [|h deck' IH]; intros board dealt.
+  - simpl. left. reflexivity.
+  - destruct h as [c1 c2 c3]. simpl.
+    destruct (should_stop (c1::c2::c3::board)) eqn:Hstop.
+    + apply stopping_condition_sound. assumption.
+    + apply IH.
+Qed.
+
+(* I think it might also be smart to add some sort of a Reset button so that users
+   can begin a new game when the game is over (or in case they give up).
+
+   Note to self: there is some neat syntactic sugar apparently for "updating"
+   fields, but I don't know if I want to add coq-record-update to this toy
+   just for that reason. *)
+Definition game_engine (s : State) (e : Event) : State :=
+  match s.(phase), e with
+  | UserSelecting selection, CardClick card =>
+    match handle_click selection card with
+    | NewSel new_selection => {| deck := s.(deck); board := s.(board); phase := UserSelecting new_selection |}
+    | TriggerCheck (mkTriple c1 c2 c3 as t) =>
+      if algebraic_validb_points c1 c2 c3
+      then {| deck := s.(deck); board := s.(board); phase := AnimatingMatch t |}
+      else {| deck := s.(deck); board := s.(board); phase := AnimatingFail t |}
+    end
+  | UserSelecting _, AnimationOver => s (* AnimationOver should not happen. *)
+  | AnimatingMatch _, CardClick _ => s (* Ignore user input during animations. *)
+  | AnimatingMatch selection, AnimationOver =>
+    let board_minus_match := remove_triple selection s.(board) in
+      let '(deck', board', dealt) := deal s.(deck) board_minus_match [] in
+        match dealt with
+        | [] => if referee board'
+                then {| deck := deck'; board := board'; phase := UserSelecting Sel0 |}
+                else {| deck := deck'; board := board'; phase := GameOver |}
+        | _ => {| deck := deck'; board := board'; phase := AnimatingDeal dealt |}
+        end
+  | AnimatingFail _, CardClick _ => s (* Ignore user input during animations. *)
+  | AnimatingFail selection, AnimationOver =>
+      {| deck := s.(deck); board := s.(board); phase := UserSelecting Sel0 |}
+  | AnimatingDeal _, CardClick _ => s (* Ignore user input during animations. *)
+  | AnimatingDeal selection, AnimationOver =>
+      if referee s.(board)
+      then {| deck := s.(deck); board := s.(board); phase := UserSelecting Sel0 |}
+      else {| deck := s.(deck); board := s.(board); phase := GameOver |}
+  | GameOver, _ => s
+  end.
